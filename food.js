9e4cfd6f8787e658d745f6db6ab9a80a6a4b529f
sujamanoj <suja@masstv.app>
2022-04-24 09:51:59 -0700

    prefinal with working Api

--- food.js
+++ food.js
@@ -1,9 +1,38 @@
-//var requestUrl ='https://foodish-api.herokuapp.com/'
+var searchbtn = document.getElementById("search-btn");
+function searchbrewries(location) {
+  var request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    `https://api.openbrewerydb.org/breweries?by_state=california&by_city=${location}`
+  );
 
-fetch('https://foodish-api.herokuapp.com/')
-.then(function(response){
-  response.text().then(function(data){
-        console.log(data);
-    })   
-  
-    });
+  request.onload = function () {
+    var response = request.response;
+    var parsedData = JSON.parse(response);
+    console.log(parsedData);
+
+    for (var i = 0; i < parsedData.length; i++) {
+      var name = parsedData[i].name;
+      var breweries = document.createElement("li");
+      breweries.innerHTML = name;
+
+      breweries.addEventListener("click", function () {
+        console.log("click");
+      });
+      document.getElementById("breweries").appendChild(breweries);
+      var website_url = parsedData[i].website_url;
+
+      var website = document.createElement("a");
+      website.setAttribute("href", website_url);
+      website.textContent = "website";
+      document.getElementById("breweries").appendChild(website);
+    }
+  };
+
+  request.send();
+}
+searchbtn.addEventListener("click", function () {
+  var inputEl = document.getElementById("location");
+  console.log(inputEl.value);
+  searchbrewries(inputEl.value);
+});
